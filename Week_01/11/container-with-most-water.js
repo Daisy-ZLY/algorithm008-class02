777c5229f2dad2caa486e4d36ee983b0e67260fd
zhangliyuan <zhangliyuan0727@163.com>
2020-04-19 19:34:57 +0800

    feat:first week homework

---
+++ Week_01/11/container-with-most-water.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+//解题思路，使用两边向中间的方式进行查找，计算盛水面积最多的容器
+// 双层遍历，时间复杂度为O(n^2)
+// var maxArea = function(height) {
+//     let area = 0;
+//     for(let i = 0;i < height.length;i++){
+//         for(let j = height.length;j--;){
+//             area = Math.max(area,(j-i) * Math.min(height[i],height[j]))
+//         }
+//     }
+//     return area;
+// };
+/**
+ * 单层遍历
+ * 1.设置两个变量，从左向右i，从右向左j； let i = 0,j = height.length-1
+ * 2.计算两个柱子之间的面积 area = (j-i) * Math.min(height(i),height(j))
+ * 3.若i下标对应的柱子高，则j向左移动(j--)，若j下标对应的柱子高，则i向右移动(i++)
+ * 4.当不满足i<j条件时，则表明循环已结束
+ */
+
+var maxArea = function (height) {
+	let area = 0;
+	let i = 0, j = height.length - 1;
+	while (i < j) {
+		area = height[i] < height[j] ? (Math.max(area, (j - i) * height[i++])) : (Math.max(area, (j - i) * height[j--]))
+	}
+	return area
+}
---
+++ Week_01/283/move-zero.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+/**
+ * 遇0删除，尾部填充
+ */
+// var moveZeroes = function(nums) {
+// 	for(var index = nums.length;index--;){
+// 		if(nums[index]=== 0){
+// 			nums.splice(index,1)
+// 			nums.push(0)
+// 		}
+// 	}
+// };
+/**
+ * 遇0位置交换,数组结构赋值
+ * 1.定义快慢两个指针，快指针i表示数组遍历下标，慢指针j表示数组内第一个值为0的下标
+ * 2.遍历数组，当值不为0，则交换i，j下标对应的值,j++
+ */
+function moveZeroes(nums){
+	for(let i = 0,j = 0;i < nums.length;i++){
+		if(nums[i] !== 0){
+			[nums[j++],nums[i]] = [nums[i],nums[j]]
+		}
+	}
+	return nums
+}
+console.log(moveZeroes([0,1,0,3,12]))
+console.log(moveZeroes([0,0,1]))
